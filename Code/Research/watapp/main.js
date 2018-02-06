0e1e8b3549157e75158ac9741c87bf2e7e68d0bb
Juan Alvarado <jalvarado91@gmail.com>
2018-02-06 18:36:42 -0500

    testing electron app to handle custom urls

---
+++ Code/Research/watapp/main.js
@@ -0,0 +1,98 @@
+const electron = require("electron");
+// Module to control application life.
+const app = electron.app;
+// Module to create native browser window.
+const BrowserWindow = electron.BrowserWindow;
+const protocol = electron.protocol;
+
+const PROTOCOL_STRING = "watapp://";
+const PROTOCOL_PREFIX = PROTOCOL_STRING.split(":")[0];
+
+const path = require("path");
+const url = require("url");
+
+// Keep a global reference of the window object, if you don't, the window will
+// be closed automatically when the JavaScript object is garbage collected.
+let mainWindow;
+
+function devToolsLog(s) {
+  if (mainWindow && mainWindow.webContents) {
+    mainWindow.webContents.executeJavaScript(`console.log("${s}")`);
+  }
+}
+
+function createWindow() {
+  // Create the browser window.
+  mainWindow = new BrowserWindow({ width: 800, height: 600 });
+  mainWindow.webContents.openDevTools();
+
+  protocol.registerHttpProtocol(
+    PROTOCOL_PREFIX,
+    (req, cb) => {
+      const reqUrl = req.url;
+      devToolsLog(req);
+      devToolsLog(`Received url: ${reqUrl}`);
+      // and load the index.html of the app.
+      mainWindow.loadURL(
+        url.format({
+          pathname: path.join(__dirname, "index.html"),
+          protocol: "file:",
+          slashes: true
+        })
+      );
+    },
+    err => {
+      if (!err) {
+        devToolsLog("registered watapp protocol");
+      } else {
+        console.error("couldn't register protocol");
+        console.error(error);
+      }
+    }
+  );
+
+  // Open the DevTools.
+  // mainWindow.webContents.openDevTools()
+
+  // Emitted when the window is closed.
+  mainWindow.on("closed", function() {
+    // Dereference the window object, usually you would store windows
+    // in an array if your app supports multi windows, this is the time
+    // when you should delete the corresponding element.
+    mainWindow = null;
+  });
+}
+
+// function registerProtocols() {
+
+// }
+
+function appReady() {
+  createWindow();
+  // registerProtocols();
+}
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.on("ready", appReady);
+
+// Quit when all windows are closed.
+app.on("window-all-closed", function() {
+  // On OS X it is common for applications and their menu bar
+  // to stay active until the user quits explicitly with Cmd + Q
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
+
+app.on("activate", function() {
+  // On OS X it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (mainWindow === null) {
+    createWindow();
+  }
+});
+
+// In this file you can include the rest of your app's specific main process
+// code. You can also put them in separate files and require them here.
