72dd82fec59b9d7b778a621202688a1d3aa444c1
Mark Fajet <mfajet@fiu.edu>
2018-03-20 15:41:47 -0400

    Run metadata extraction on example output

--- Code/Analysis/extract_metadata_wat.py
+++ Code/Analysis/extract_metadata_wat.py
@@ -8,7 +8,7 @@ m = MetadataExtractor()
 # print m.extract_data_from_wat(wat_file)
 # print m.extract_data_from_html(html_file)
 #
-dir_name="/run/media/mfajet/Data/projects/Web-Page-Archiving-and-Content-Analysis-1.0/Code/watapp/outdir/files/"
+dir_name="../Reference Work/example_output"
 f = open('test.csv','w')
 mydict={
     "date":"",
@@ -19,13 +19,33 @@ mydict={
 }
 w = csv.DictWriter(f,mydict.keys())
 w.writeheader()
-for filename in os.listdir(dir_name):
-    if filename.endswith(".wat"):
-        d = m.extract_data_from_wat(os.path.join(dir_name,filename))
-        if not ("date" in d and "title" in d and "header" in d and "publisher" in d and "author" in d):
-            print filename
-        w.writerow(d)
-        continue
-    else:
-        continue
+
+for folder, subs, files in os.walk(dir_name):
+        for filename in files:
+            if filename.endswith(".wat"):
+                d = m.extract_data_from_wat(os.path.join(foler,filename))
+                if not ("date" in d and "title" in d and "header" in d and "publisher" in d and "author" in d):
+                    print filename
+                if not date == {}:
+                    w.writerow(d)
+                continue
+            elif filename.endswith(".html"):
+                d = m.extract_data_from_html(os.path.join(folder,filename))
+                if not ("date" in d and "title" in d and "header" in d and "publisher" in d and "author" in d):
+                    print filename
+                if not d == {} and not d["title"] == "ns":
+                    w.writerow(d)
+                continue
+            else:
+                continue
+
+# for filename in os.listdir(dir_name):
+#     if filename.endswith(".wat"):
+#         d = m.extract_data_from_wat(os.path.join(dir_name,filename))
+#         if not ("date" in d and "title" in d and "header" in d and "publisher" in d and "author" in d):
+#             print filename
+#         w.writerow(d)
+#         continue
+#     else:
+#         continue
 f.close()
