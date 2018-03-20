d73223d322e273255d5e5fdaef99b861168452d9
Mark Fajet <mfajet@fiu.edu>
2018-03-19 21:59:05 -0400

    test file to make csv out of directory of wat files

---
+++ Code/Analysis/extract_metadata_wat.py
@@ -0,0 +1,31 @@
+from metadata_extractor import MetadataExtractor
+import os
+import csv
+# wat_file="/run/media/mfajet/Data/projects/Web-Page-Archiving-and-Content-Analysis-1.0/Code/watapp/outdir/files/15196120301161_theverge.com.wat"
+# html_file="/run/media/mfajet/Data/projects/Web-Page-Archiving-and-Content-Analysis-1.0/Code/watapp/outdir/files/15196989105181_tass.com_2/988157.html"
+
+m = MetadataExtractor()
+# print m.extract_data_from_wat(wat_file)
+# print m.extract_data_from_html(html_file)
+#
+dir_name="/run/media/mfajet/Data/projects/Web-Page-Archiving-and-Content-Analysis-1.0/Code/watapp/outdir/files/"
+f = open('test.csv','w')
+mydict={
+    "date":"",
+    "author":"",
+    "header":"",
+    "publisher":"",
+    "title":""
+}
+w = csv.DictWriter(f,mydict.keys())
+w.writeheader()
+for filename in os.listdir(dir_name):
+    if filename.endswith(".wat"):
+        d = m.extract_data_from_wat(os.path.join(dir_name,filename))
+        if not ("date" in d and "title" in d and "header" in d and "publisher" in d and "author" in d):
+            print filename
+        w.writerow(d)
+        continue
+    else:
+        continue
+f.close()
