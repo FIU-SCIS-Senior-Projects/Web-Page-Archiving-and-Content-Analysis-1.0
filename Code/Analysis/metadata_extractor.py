67b69c053bb257070f5f6ec0da3cef23ad5c426b
Mark Fajet <mfajet@fiu.edu>
2018-03-19 21:12:08 -0400

    Added more extraction methods

--- Code/Analysis/metadata_extractor.py
+++ Code/Analysis/metadata_extractor.py
@@ -29,34 +29,81 @@ class MetadataExtractor:
         lambda: self.article.find("meta", attrs={'name':'sailthru.title'}).get("content", None),
         lambda: self.article.find("meta", attrs={'name':'dc.title'}).get("content", None),
         lambda: self.article.find("meta", attrs={'name':'DC.title'}).get("content", None),
-        lambda: self.article.find("meta", attrs={'name':'title'}).get("content", None)
+        lambda: self.article.find("meta", attrs={'name':'title'}).get("content", None),
+        lambda: self.article.find("meta",  property="og:title")["content"],
+        lambda: self.article.find("meta", attrs={'name':'twitter:title'} ).get("value", None),
+        lambda: self.article.find("meta", attrs={'name':'sailthru.title'}).get("value", None),
+        lambda: self.article.find("meta", attrs={'name':'dc.title'}).get("value", None),
+        lambda: self.article.find("meta", attrs={'name':'DC.title'}).get("value", None),
+        lambda: self.article.find("meta", attrs={'name':'title'}).get("value", None),
+        lambda: self.article.find("meta", attrs={'name':'mrc__share_title'} ).get("value", None),
+        lambda: self.article.find("meta", attrs={'name':'mrc__share_title'} ).get("content", None),
         ]
         self.save_first(title_methods,"title")
+
     def get_author(self):
         author_methods=[
             lambda: self.article.find("meta",  property="author")["content"],
-            lambda: self.article.find("meta",  property="self.article:author")["content"],
+            lambda: self.article.find("meta",  property="article:author")["content"],
             lambda: self.article.find("meta", attrs={'name':'sailthru.author'}).get("content", None),
             lambda: self.article.find("meta", attrs={'name':'author'}).get("content", None),
             lambda: self.article.find("meta", attrs={'name':'dc.creator'}).get("content", None),
             lambda: self.article.find("meta", attrs={'name':'DC.creator'}).get("content", None),
             lambda: self.article.find("meta", attrs={'name':'dc.contributor'}).get("content", None),
             lambda: self.article.find("meta", attrs={'name':'DC.contributor'}).get("content", None),
-            lambda: self.article.find("meta", attrs={'name':'twitter:creator'} ).get("content", None)
+            lambda: self.article.find("meta", attrs={'name':'twitter:creator'} ).get("content", None),
+            lambda: self.article.find("meta", property="author")["value"],
+            lambda: self.article.find("meta", property="article:author")["value"],
+            lambda: self.article.find("meta", attrs={'name':'sailthru.author'}).get("value", None),
+            lambda: self.article.find("meta", attrs={'name':'author'}).get("value", None),
+            lambda: self.article.find("meta", attrs={'name':'dc.creator'}).get("value", None),
+            lambda: self.article.find("meta", attrs={'name':'DC.creator'}).get("value", None),
+            lambda: self.article.find("meta", attrs={'name':'dc.contributor'}).get("value", None),
+            lambda: self.article.find("meta", attrs={'name':'DC.contributor'}).get("value", None),
+            lambda: self.article.find("meta", attrs={'name':'twitter:creator'} ).get("value", None),
+            lambda: self.article.find("meta", attrs={'name':'mediator_author'} ).get("value", None),
+            lambda: self.article.find("meta", attrs={'name':'mediator_author'} ).get("content", None),
         ]
         self.save_first(author_methods,"author")
 
     def get_date(self):
         date_methods=[
             lambda: self.article.find("meta",  property="date")["content"],
-            lambda: self.article.find("meta",  property="self.article:published_time")["content"],
-            lambda: self.article.find("meta",  property="self.article:modified_time")["content"],
+            lambda: self.article.find("meta",  property="article:published_time")["content"],
+            lambda: self.article.find("meta",  property="article:modified_time")["content"],
+            lambda: self.article.find("meta",  property="article:published")["content"],
+            lambda: self.article.find("meta",  property="article:modified")["content"],
             lambda: self.article.find("meta", attrs={'name':'sailthru.date'}).get("content", None),
             lambda: self.article.find("meta", attrs={'name':'dc.date'}).get("content", None),
             lambda: self.article.find("meta", attrs={'name':'dc.date.issued'}).get("content", None),
             lambda: self.article.find("meta", attrs={'name':'DC.date'}).get("content", None),
             lambda: self.article.find("meta", attrs={'name':'DC.date.issued'}).get("content", None),
-            lambda: self.article.find("meta", attrs={'name':'date'}).get("content", None)
+            lambda: self.article.find("meta", attrs={'name':'date'}).get("content", None),
+            lambda: self.article.find("meta", attrs={'name':'ptime'}).get("content", None),
+            lambda: self.article.find("meta", attrs={'name':'utime'}).get("content", None),
+            lambda: self.article.find("meta", attrs={'name':'pdate'}).get("content", None),
+            lambda: self.article.find("meta", attrs={'name':'DISPLAYDATE'}).get("content", None),
+            lambda: self.article.find("meta",  property="date")["value"],
+            lambda: self.article.find("meta",  property="article:published_time")["value"],
+            lambda: self.article.find("meta",  property="article:modified_time")["value"],
+            lambda: self.article.find("meta",  property="article:published")["value"],
+            lambda: self.article.find("meta",  property="article:modified")["value"],
+            lambda: self.article.find("meta", attrs={'name':'sailthru.date'}).get("value", None),
+            lambda: self.article.find("meta", attrs={'name':'dc.date'}).get("value", None),
+            lambda: self.article.find("meta", attrs={'name':'dc.date.issued'}).get("value", None),
+            lambda: self.article.find("meta", attrs={'name':'DC.date'}).get("value", None),
+            lambda: self.article.find("meta", attrs={'name':'DC.date.issued'}).get("value", None),
+            lambda: self.article.find("meta", attrs={'name':'date'}).get("value", None),
+            lambda: self.article.find("meta", attrs={'name':'ptime'}).get("value", None),
+            lambda: self.article.find("meta", attrs={'name':'utime'}).get("value", None),
+            lambda: self.article.find("meta", attrs={'name':'pdate'}).get("value", None),
+            lambda: self.article.find("meta", attrs={'name':'DISPLAYDATE'}).get("value", None),
+            lambda: self.article.find("meta", attrs={'name':'published_time_telegram'}).get("value", None),
+            lambda: self.article.find("meta", attrs={'name':'published_time_telegram'}).get("content", None),
+            lambda: self.article.find("meta", attrs={'name':'mediator_published_time'}).get("value", None),
+            lambda: self.article.find("meta", attrs={'name':'mediator_published_time'}).get("content", None),
+            lambda: self.article.find("meta", attrs={'name':'publish_date'}).get("value", None),
+            lambda: self.article.find("meta", attrs={'name':'publish_date'}).get("content", None),
         ]
         self.save_first(date_methods,"date")
 
@@ -68,21 +115,37 @@ class MetadataExtractor:
             lambda: self.article.find("meta", attrs={'name':'twitter:description'} ).get("content", None),
             lambda: self.article.find("meta", attrs={'name':'sailthru.description'}).get("content", None),
             lambda: self.article.find("meta", attrs={'name':'dc.description'}).get("content", None),
-            lambda: self.article.find("meta", attrs={'name':'DC.description'}).get("content", None)
+            lambda: self.article.find("meta", attrs={'name':'DC.description'}).get("content", None),
+            lambda: self.article.find("meta", attrs={'name':'description'}).get("value", None),
+            lambda: self.article.find("meta",  property="og:description")["value"],
+            lambda: self.article.find("meta",  property="description")["value"],
+            lambda: self.article.find("meta", attrs={'name':'twitter:description'} ).get("value", None),
+            lambda: self.article.find("meta", attrs={'name':'sailthru.description'}).get("value", None),
+            lambda: self.article.find("meta", attrs={'name':'dc.description'}).get("value", None),
+            lambda: self.article.find("meta", attrs={'name':'DC.description'}).get("value", None),
+            lambda: self.article.find("meta", attrs={'name':'mrc__share_description'}).get("content", None),
+            lambda: self.article.find("meta", attrs={'name':'mrc__share_description'}).get("value", None),
         ]
         self.save_first(header_methods,"header")
 
     def get_publisher(self):
         publisher_methods=[
-            lambda: self.article.find("meta",  property="self.article:publisher")["content"],
+            lambda: self.article.find("meta",  property="article:publisher")["content"],
             lambda: self.article.find("meta",  property="publisher")["content"],
             lambda: self.article.find("meta",  property="og:site_name")["content"],
             lambda: self.article.find("meta", attrs={'name':'twitter:site'} ).get("content", None),
             lambda: self.article.find("meta", attrs={'name':'sailthru.description'}).get("content", None),
             lambda: self.article.find("meta", attrs={'name':'dc.publisher'}).get("content", None),
-            lambda: self.article.find("meta", attrs={'name':'DC.publisher'}).get("content", None)
+            lambda: self.article.find("meta", attrs={'name':'DC.publisher'}).get("content", None),
+            lambda: self.article.find("meta",  property="article:publisher")["value"],
+            lambda: self.article.find("meta",  property="publisher")["value"],
+            lambda: self.article.find("meta",  property="og:site_name")["value"],
+            lambda: self.article.find("meta", attrs={'name':'twitter:site'} ).get("value", None),
+            lambda: self.article.find("meta", attrs={'name':'sailthru.description'}).get("value", None),
+            lambda: self.article.find("meta", attrs={'name':'dc.publisher'}).get("value", None),
+            lambda: self.article.find("meta", attrs={'name':'DC.publisher'}).get("value", None)
         ]
-        self.save_first(publisher_methods,"header")
+        self.save_first(publisher_methods,"publisher")
 
     def extract_data_from_html(self, file_name):
         self.data={}
